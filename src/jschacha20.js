1f52cb482f11a978a86a1f21b689aa67e5a23789
Andy Wang <cbeuw.andy@gmail.com>
2019-12-25 15:48:46 +0000

    Implement chacha20

---
+++ src/jschacha20.js
@@ -0,0 +1,193 @@
+/**
+ * Construct Chacha20 instance with key and nonce
+ * Key should be Uint8Array with 32 bytes
+ * None should be Uint8Array with 8 bytes
+ *
+ *
+ * @throws {Error}
+ * @param {Uint8Array} key
+ * @param {Uint8Array} nonce
+ */
+var JSChacha20 = function (key, nonce) {
+    if (!(key instanceof Uint8Array) || key.length !== 32) {
+      throw new Error('Key should be 32 byte array!')
+    }
+  
+    if (!(nonce instanceof Uint8Array) || nonce.length !== 8) {
+      throw new Error('Nonce should be 8 byte array!')
+    }
+  
+    this.rounds = 20
+    this.sigma = [0x61707865, 0x3320646e, 0x79622d32, 0x6b206574]
+    this.param = [
+      // Constant
+      this.sigma[0],
+      this.sigma[1],
+      this.sigma[2],
+      this.sigma[3],
+      // Key
+      _get32(key, 0),
+      _get32(key, 4),
+      _get32(key, 8),
+      _get32(key, 12),
+
+      _get32(key, 16),
+      _get32(key, 20),
+      _get32(key, 24),
+      _get32(key, 28),
+
+      // Counter
+      0,
+      0,
+      // Nonce
+      _get32(nonce, 0),
+      _get32(nonce, 4),
+    ]
+  
+    // init block 64 bytes //
+    this.block = [
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    ]
+  
+    // internal byte counter //
+    this.byteCounter = 0
+  }
+  
+  /**
+   *  Encrypt or Decrypt data with key and nonce
+   *
+   * @param {Uint8Array} data
+   * @return {Uint8Array}
+   * @private
+   */
+  JSChacha20.prototype._update = function (data) {
+    if (!(data instanceof Uint8Array) || data.length === 0) {
+      throw new Error('Data should be type of bytes (Uint8Array) and not empty!')
+    }
+  
+    var output = new Uint8Array(data.length)
+  
+    // core function, build block and xor with input data //
+    for (var i = 0; i < data.length; i++) {
+      if (this.byteCounter === 0 || this.byteCounter === 64) {
+        this._chacha()
+        this._counterIncrement()
+        this.byteCounter = 0
+      }
+  
+      output[i] = data[i] ^ this.block[this.byteCounter++]
+    }
+  
+    return output
+  }
+  /**
+   *  Encrypt data with key and nonce
+   *
+   * @param {Uint8Array} data
+   * @return {Uint8Array}
+   */
+  JSChacha20.prototype.encrypt = function (data) {
+    return this._update(data)
+  }
+  
+  /**
+   *  Decrypt data with key and nonce
+   *
+   * @param {Uint8Array} data
+   * @return {Uint8Array}
+   */
+  JSChacha20.prototype.decrypt = function (data) {
+    return this._update(data)
+  }
+  
+  JSChacha20.prototype._counterIncrement = function () {
+    // Max possible blocks is 2^64
+    this.param[12] = (this.param[12] + 1) >>> 0
+    if (this.param[12] === 0) {
+      this.param[13] = (this.param[13] + 1) >>> 0
+    }
+  }
+  
+  JSChacha20.prototype._chacha = function () {
+    var mix = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    var i = 0
+    var b = 0
+  
+    const qr = function (a, b, c, d) {
+      mix[a] = (mix[a] + mix[b]) >>> 0
+      mix[d] ^= mix[a]
+      mix[d] = _rotl(mix[d], 16) >>> 0
+
+      mix[c] = (mix[c] + mix[d]) >>> 0
+      mix[b] ^= mix[c]
+      mix[b] = _rotl(mix[b], 12) >>> 0
+
+      mix[a] = (mix[a] + mix[b]) >>> 0
+      mix[d] ^= mix[a]
+      mix[d] = _rotl(mix[d], 8) >>> 0
+
+      mix[c] = (mix[c] + mix[d]) >>> 0
+      mix[b] ^= mix[c]
+      mix[b] = _rotl(mix[b], 7) >>> 0
+    }
+  
+    // copy param array to mix //
+    for (i = 0; i < 16; i++) {
+      mix[i] = this.param[i]
+    }
+  
+    // mix rounds //
+    for (i = 0; i < this.rounds; i += 2) {
+      qr(0, 4, 8, 12)
+      qr(1, 5, 9, 13)
+      qr(2, 6, 10, 14)
+      qr(3, 7, 11, 15)
+      qr(0, 5, 10, 15)
+      qr(1, 6, 11, 12)
+      qr(2, 7, 8, 13)
+      qr(3, 4, 9, 14)
+    }
+  
+    for (i = 0; i < 16; i++) {
+      // add
+      mix[i] += this.param[i]
+  
+      // store
+      this.block[b++] = mix[i] & 0xFF
+      this.block[b++] = (mix[i] >>> 8) & 0xFF
+      this.block[b++] = (mix[i] >>> 16) & 0xFF
+      this.block[b++] = (mix[i] >>> 24) & 0xFF
+    }
+  }
+  
+  /**
+   * Little-endian to uint 32 bytes
+   *
+   * @param {Uint8Array|[number]} data
+   * @param {number} index
+   * @return {number}
+   * @private
+   */
+  const _get32 = function (data, index) {
+    return data[index++] ^ (data[index++] << 8) ^ (data[index++] << 16) ^ (data[index] << 24)
+  }
+  
+  /**
+   * Cyclic left rotation
+   *
+   * @param {number} data
+   * @param {number} shift
+   * @return {number}
+   * @private
+   */
+  const _rotl = function (data, shift) {
+    return ((data << shift) | (data >>> (32 - shift)))
+  }
+  
+  // EXPORT //
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = JSChacha20
+  }
---
+++ test/jschacha20.test.js
@@ -0,0 +1,54 @@
+'use strict'
+/**
+ * Created by Mykola Bubelich
+ * 2017-01-25
+ */
+
+import test from 'tape'
+import JSChacha20 from '../src/jschacha20'
+
+/**
+ * Encrypt / Decrypt
+ */
+test('Encrypt and decrypt for 256 byte should be same', tape => {
+  const crypto = require('crypto')
+
+  const key = new Uint8Array(crypto.randomBytes(32))
+  const nonce = new Uint8Array(crypto.randomBytes(8))
+  const data = new Uint8Array(crypto.randomBytes(4096))
+
+  const encoder = new JSChacha20(key, nonce)
+  const decoder = new JSChacha20(key, nonce)
+
+  const encr = encoder.encrypt(data)
+  const decr = decoder.decrypt(encr)
+
+  tape.deepEqual(encoder.param, decoder.param, 'Parameters should be equivalent')
+  tape.deepEqual(data, decr, 'Decrypted data should be the same as input')
+  tape.deepEqual([64, 64], [encoder.param[12], decoder.param[12]], 'Counter should be equal 64')
+
+  tape.end()
+})
+
+// https://tools.ietf.org/html/draft-agl-tls-chacha20poly1305-04#section-7
+test('Key stream should be equal to the reference', tape => {
+  const nullBlock = new Uint8Array(Array(64).fill(0))
+
+  const key = new Uint8Array(Array(32).fill(0))
+
+  const nonce = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0])
+
+  const cipher = new JSChacha20(key, nonce)
+
+  const expStream = new Uint8Array([
+    0x76, 0xb8, 0xe0, 0xad, 0xa0, 0xf1, 0x3d, 0x90, 0x40, 0x5d, 0x6a, 0xe5, 0x53, 0x86, 0xbd, 0x28,
+    0xbd, 0xd2, 0x19, 0xb8, 0xa0, 0x8d, 0xed, 0x1a, 0xa8, 0x36, 0xef, 0xcc, 0x8b, 0x77, 0x0d, 0xc7, 
+    0xda, 0x41, 0x59, 0x7c, 0x51, 0x57, 0x48, 0x8d, 0x77, 0x24, 0xe0, 0x3f, 0xb8, 0xd8, 0x4a, 0x37, 
+    0x6a, 0x43, 0xb8, 0xf4, 0x15, 0x18, 0xa1, 0x1c, 0xc3, 0x87, 0xb6, 0x69, 0xb2, 0xee, 0x65, 0x86
+  ])
+
+  tape.deepEqual(cipher.encrypt(nullBlock), expStream, 'Key stream should be equal to the reference')
+
+  tape.end()
+})
+
